5880c1de268968d81bb4e727802ae8b7ba3b623c
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-03-07 12:51:16 +0100

    Cleanup annotations

--- src/Console.php
+++ src/Console.php
@@ -10,12 +10,21 @@
 
 namespace SebastianBergmann\Environment;
 
-/**
- */
 class Console
 {
+    /**
+     * @var int
+     */
     const STDIN  = 0;
+
+    /**
+     * @var int
+     */
     const STDOUT = 1;
+
+    /**
+     * @var int
+     */
     const STDERR = 2;
 
     /**
