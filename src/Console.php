bc3445fa3cc48d5e55e469ec9c6c53a5d2333a8a
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2018-10-23 12:25:36 +0200

    Add description for our edge case

--- src/Console.php
+++ src/Console.php
@@ -83,6 +83,9 @@ final class Console
     /**
      * Returns if the file descriptor is an interactive terminal or not.
      *
+     * Normally, we want to use a resource as a parameter, yet sadly it's not always awailable,
+     * eg when running code in interactive console (`php -a`), STDIN/STDOUT/STDERR constants are not defined.
+     *
      * @param int|resource $fileDescriptor
      */
     public function isInteractive($fileDescriptor = self::STDOUT): bool
