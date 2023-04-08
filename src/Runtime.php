66ef87421a63a7a8d114301a5c2b12eec96f5219
Joan Miquel <joanhey@kumbiaphp.com>
2023-04-08 03:35:50 +0200

    Fix opcache.jit validation

--- src/Runtime.php
+++ src/Runtime.php
@@ -73,7 +73,13 @@ final class Runtime
             return false;
         }
 
-        if (strpos(ini_get('opcache.jit'), '0') === 0) {
+        $jit = ini_get('opcache.jit');
+
+        if (($jit === 'disable') || ($jit === 'off')) {
+            return false;
+        }
+
+        if (strrpos(ini_get('opcache.jit'), '0') === 3) {
             return false;
         }
 
